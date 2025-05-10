4de0fd740350a61efd5ee6229a08cfcfd1c05ff8
yousefmarey12 <yousef.marey@ucalgary.ca>
2025-05-10 19:00:25 +0300

    second ssss

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -22,7 +22,10 @@ CREATE TABLE Airline (
 CREATE TABLE Hotel (
 	hotel_id int primary key,
 	hotel_location NVARCHAR(40),
-	hotel_name NVARCHAR(30)
+	hotel_name NVARCHAR(30),
+	package_id int,
+	CONSTRAINT constra FOREIGN KEY (package_id) 
+    REFERENCES travel_package (package_id)
 )
 
 -- add Check for age and address
@@ -37,12 +40,15 @@ CREATE TABLE LocalGuide (
 
 )
 
+-- make local_guide_id as a FK
 CREATE TABLE TouristAttraction (
 	id int primary key,
 	tourist_attr_location nvarchar(40),
 	title nvarchar(40),
 	tor_description nvarchar(100),
-
+	local_guide_id int,
+	CONSTRAINT constra FOREIGN KEY (local_guide_id) 
+    REFERENCES LocalGuide (job_id)
 
 )
 
@@ -51,5 +57,26 @@ CREATE TABLE travel_package (
 	package_id int primary key,
 	package_name nvarchar(30),
 	cost float,
-	duration 
+	duration int,
+
 )
+
+
+-- make sure its 1, 2, 3, 4, 5
+-- make composite primary key with hotel_id and author
+CREATE TABLE Rating (
+	hotel_id int,
+	author nvarchar(30),
+	comment nvarchar(100),
+	number_of_stars int,
+	CONSTRAINT constra FOREIGN KEY (hotel_id) 
+    REFERENCES Hotel (hotel_id)
+)
+
+ALTER TABLE Rating
+ADD CONSTRAINT PK_1
+PRIMARY KEY (hotel_id, author)
+
+
+ALTER TABLE RATING
+ADD num_of_star char(1) CHECK (num_of_star IN ('1', '2', '3', '4', '5'))
